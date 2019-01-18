8efac3542dfb140e2d79c5efbe2f6978de067109
Kirill-Mokhov <mokhovichenkokirill@gmail.com>
2019-01-19 01:13:29 +1000

    gt

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
+#include <cstring>
 #include "src/base64.hpp"
+#include "gtest/gtest.h"
+
+using namespace std;
 
 const char OPT_ENCODE[] = "-e";
 const char OPT_DECODE[] = "-d";
 
 int main(int argc, char *argv[]) {
+    //char *s = base64_decode((const char*)"dXBkYXRpbmc=");
+
     if (argc == 1 || argc > 3) {
         throw std::invalid_argument("No arguments provided");
     }
 
     if (argc == 2) {
+        std::cout << endl;
         std::cout << base64_encode(argv[1]);
     }
 
